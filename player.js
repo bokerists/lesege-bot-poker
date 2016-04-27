54733699d2d284cfce8c8fabe43d7c2790ddcaff
mircolac <mirco.lacalandra@gmail.com>
2016-04-27 20:17:24 +0200

    pdz

--- player.js
+++ player.js
@@ -4,23 +4,44 @@ exports = module.exports = {
     VERSION: 'Superstar poker js-player',
 
     bet: function (gamestate) {
+
+        var casoBase = function() {
+            if (gamestate.players[gamestate.me].cards[0].rank === gamestate.players[gamestate.me].cards[1].rank) {
+                amount = gamestate.callAmount
+            } else if
+            (gamestate.players[gamestate.me].cards[0].rank === "A" || gamestate.players[gamestate.me].cards[1].rank === "A") {
+                amount = gamestate.callAmount
+            } else if (gamestate.players[gamestate.me].cards[0].rank === "K" || gamestate.players[gamestate.me].cards[1].rank === "K") {
+                amount = gamestate.callAmount
+            } else if (gamestate.players[gamestate.me].cards[0].type === gamestate.players[gamestate.me].cards[1].type) {
+                amount = gamestate.callAmount
+            }
+
+        }
+        
         var amount = 0
 
-        if (gamestate.players[gamestate.me].cards[0].rank === gamestate.players[gamestate.me].cards[1].rank) {
-            amount = gamestate.callAmount
-        } else if
-        (gamestate.players[gamestate.me].cards[0].rank === "A" || gamestate.players[gamestate.me].cards[1].rank === "A") {
-            amount = gamestate.callAmount
-        } else if (gamestate.players[gamestate.me].cards[0].rank === "K" || gamestate.players[gamestate.me].cards[1].rank === "K") {
-            amount = gamestate.callAmount
-        }  else if (gamestate.players[gamestate.me].cards[0].type  === gamestate.players[gamestate.me].cards[1].type) {
-            amount = gamestate.callAmount
+        if(gamestate.commonCards.length == 0) {
+            casoBase()
+        }
+        else {
+            var flag = 0
+            gamestate.commonCards.forEach(function (carta) {
+                if (carta.type === gamestate.players[gamestate.me].cards[0].type || carta.type === gamestate.players[gamestate.me].cards[1].type) {
+                    flag = 1
+                }
+            })
+            if (flag) {
+                amount = gamestate.callAmount*2
+            } else {
+                amount = 0
+            }
         }
 
-
-    
       return amount
   }
+
+
 
 
 /*
@@ -35,5 +56,4 @@ exports = module.exports = {
      default code block
      }
 */
-
 };
