f73eed91f247d3a69d6bf0222b7518c5af7bc78b
mircolac <mirco.lacalandra@gmail.com>
2016-04-27 19:58:35 +0200

    same type

--- player.js
+++ player.js
@@ -11,9 +11,29 @@ exports = module.exports = {
         } else if
         (gamestate.players[gamestate.me].cards[0].rank === "A" || gamestate.players[gamestate.me].cards[1].rank === "A") {
             amount = gamestate.callAmount
+        } else if (gamestate.players[gamestate.me].cards[0].rank === "K" || gamestate.players[gamestate.me].cards[1].rank === "K") {
+            amount = gamestate.callAmount
+        }  else if (gamestate.players[gamestate.me].cards[0].type  === gamestate.players[gamestate.me].cards[1].type) {
+            amount = gamestate.callAmount
         }
+
+
     
       return amount
   }
+
+
+/*
+     switch(expression) {
+     case n:
+
+     break;
+     case n:
+     code block
+     break;
+     default:
+     default code block
+     }
+*/
 
 };
